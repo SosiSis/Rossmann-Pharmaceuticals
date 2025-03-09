0758680720eceac8b0fec6b1e9df02061d68367b
SosiSis <sosinasisay30@gmail.com>
2025-03-09 20:49:32 +0300

    doing the model

--- scripts/dataLoadAndMerge.py
+++ scripts/dataLoadAndMerge.py
@@ -1,7 +1,6 @@
 import pandas as pd # type: ignore
 import numpy as np # type: ignore
-import matplotlib.pyplot as plt # type: ignore
-import seaborn as sns # type: ignore
+
 import logging
 
 # # Setup logging
